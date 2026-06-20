d4c39582097a9d0317d4123ca3da0eb51b6e1fd9
Dwight Malcolm <info@maltechenterprises.com>
2026-06-20 17:48:10 -0400

    Simplify RootLayout to isolate React 418 error

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,24 +1,17 @@
-import { useEffect } from "react";
-import { CompanyProvider } from "../context/CompanyContext";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export default function RootLayout() {
- return (
-  <Stack initialRouteName="index">
-    <Stack.Screen
-      name="index"
-      options={{ headerShown: false }}
-    />
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
 
-    <Stack.Screen
-      name="dashboard"
-      options={{ headerShown: false }}
-    />
-  </Stack>
-);
+      <Stack.Screen
+        name="dashboard"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  );
 }
